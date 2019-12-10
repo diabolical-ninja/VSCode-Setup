758f29f0f9d00395e209bd851ade485bb2338899
yassin-eltahir <15998949+diabolical-ninja@users.noreply.github.com>
2019-12-10 22:52:56 +1100

    Initial Commit

---
+++ package_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Desired extensions
+wanted_extensions=(
+    "njpwerner.autodocstring"
+    "HookyQR.beautify"
+    "joshpeng.theme-charcoal-oceanicnext"
+    "formulahendry.code-runner"
+    "spywhere.guides"
+    "ms-python.python"
+    "vscode-icons-team.vscode-icons"
+    "jebbs.plantuml"
+)
+
+# Get currently installed extensions
+current_extensions=$(`code --list-extensions`)
+
+# Install desired extensions
+for i in ${current_extensions}; do
+    if [[ " ${wanted_extensions[*]} " != *$i* ]]; then
+        code --install-extension $i
+        echo
+    fi
+done
+
+
+# Assign keyboard shortcuts, assuming default location
+cp keybindings.json "$HOME/Library/Application Support/Code/User/"
+
+# Assign Settings
+cp settings.json "$HOME/Library/Application Support/Code/User/"
